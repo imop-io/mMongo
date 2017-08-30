a7dadd3e987b7ab58d4f89a34a398974fd92db53
songww <sww4718168@163.com>
2017-08-30 16:19:53 +0800

    Add setup.py and requirements

---
+++ mmongo/__init__.py
@@ -0,0 +1,3 @@
+# coding: utf8
+
+_version = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+from mmongo import _version
+
+setup(
+    name=_project,
+    version=_version,
+    license=_license,
+    description='A python ODM of mongo based on motor',
+    long_description='',
+    platforms=('Any'),
+    keywords = ["asyncio", "motor", "mongo"]
+
+    author='songww',
+    author_email='sww4718168@gmail.com',
+    url='https://github.com/ioimop/mMongo',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3 License',
+        'Natural Language :: English',
+        'Natural Language :: Chinese',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ],
+
+    py_modules=['mmongo'],
+    install_requires=['motor'],
+)
